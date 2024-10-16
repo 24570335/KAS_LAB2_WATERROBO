07655124f76a8fb3bfb1fd668a57a3ed7d0f04e5
DESKTOP-VQPT8O8\alexe <alexevergara@gmail.com>
2024-10-16 18:56:55 +1100

    ply files now work somewhat (add colour via blender to makeit work)

--- @BrushBot/BrushBot.m
+++ @BrushBot/BrushBot.m
@@ -1,46 +1,27 @@
 classdef BrushBot < RobotBaseClass
-    %% UR5 Universal Robot 5kg payload robot model
-    %
-    % WARNING: This model has been created by UTS students in the subject
-    % 41013. No guarentee is made about the accuracy or correctness of the
-    % of the DH parameters of the accompanying ply files. Do not assume
-    % that this matches the real robot!
 
     properties(Access = public)   
-        plyFileNameStem = 'UR5';
+        plyFileNameStem = 'BrushBot'
     end
-    
+   
     
     methods
 %% Constructor
-        function self = BrushBot(baseTr,useTool,toolFilename)
-            if nargin < 3
-                if nargin == 2
-                    error('If you set useTool you must pass in the toolFilename as well');
-                elseif nargin == 0 % Nothing passed
-                    baseTr = transl(0,0,0);  
-                end             
-            else % All passed in 
-                self.useTool = useTool;
-                toolTrData = load([toolFilename,'.mat']);
-                self.toolTr = toolTrData.tool;
-                self.toolFilename = [toolFilename,'.ply'];
-            end
-          
+        function self = BrushBot(baseTr)
             self.CreateModel();
-			self.model.base = self.model.base.T * baseTr;
-            self.model.tool = self.toolTr;
+            if nargin == 1			
+				self.model.base = self.model.base.T * baseTr;
+            end          
             self.PlotAndColourRobot();
-            drawnow
         end
 
 %% CreateModel
         function CreateModel(self)
             link(1) = Link('d',0.089159,    'a',0,      'alpha', pi/2 ,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
-            link(2) = Link('d',0,         'a',-0.225,  'alpha',0,'offset',0,'qlim',[deg2rad(-90),deg2rad(90)]);
-            link(3) = Link('d',0,         'a',-0.2,'alpha',0,'offset',0,'qlim',[deg2rad(-170),deg2rad(170)]);
-            link(4) = Link('d',0,     'a',-0.2,      'alpha',0,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
-            link(5) = Link('d',0,     'a',-0.2,      'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+            link(2) = Link('d',-0.08,         'a',-0.225,  'alpha',0,'offset',0,'qlim',[deg2rad(-90),deg2rad(90)]);
+            link(3) = Link('d',0.08,         'a',-0.2,'alpha',0,'offset',0,'qlim',[deg2rad(-170),deg2rad(170)]);
+            link(4) = Link('d',-0.08,     'a',-0.2,      'alpha',0,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+            link(5) = Link('d',0.8,     'a',-0.2,      'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
             self.model = SerialLink(link,'name',self.name);            
         end    
     end
--- Wash_script.m
+++ Wash_script.m
@@ -8,11 +8,11 @@ r = UR5_adjusted;
                  0,0,0;
                  0,0,0;];
 
-for i = 1:size(waypoints, 1)
-    %pickup bottle
-    %move to bottle location
-    %orient bottle to direction of 2nd robot
-    %2nd robot to open cap
-    %2nd robot puts down cap and returns to clean bottle
-    %bottle is then put aside
-end
+% for i = 1:size(waypoints, 1)
+%     %pickup bottle
+%     %move to bottle location
+%     %orient bottle to direction of 2nd robot
+%     %2nd robot to open cap
+%     %2nd robot puts down cap and returns to clean bottle
+%     %bottle is then put aside
+% end
