134ad6b3c7d2eb2f5566e8c759b4303cb81f2cea
Sophie <sophie.l.vuillemin@student.uts.edu.au>
2024-10-18 22:32:04 +1100

    Accidentally deleted previous wash_script, reuploading it

---
+++ Wash_script.m
@@ -0,0 +1,20 @@
+hold on
+kitchenEnvironment;
+r = UR5_adjusted;
+    waypoints = [0,0,0;
+                0,0,0;
+                0,0,0;];
+    waypoints2 = [0,0,0;
+                 0,0,0;
+                 0,0,0;];
+br = BrushBot;
+
+
+% for i = 1:size(waypoints, 1)
+%     %pickup bottle
+%     %move to bottle location
+%     %orient bottle to direction of 2nd robot
+%     %2nd robot to open cap
+%     %2nd robot puts down cap and returns to clean bottle
+%     %bottle is then put aside
+% end
