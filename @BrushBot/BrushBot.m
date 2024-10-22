80e6ea22c4f653292a4b401460a422b76f814e0e
DESKTOP-VQPT8O8\alexe <alexevergara@gmail.com>
2024-10-22 12:38:02 +1100

    fixed ur3e

--- @BrushBot/BrushBot.m
+++ @BrushBot/BrushBot.m
@@ -8,13 +8,25 @@ classdef BrushBot < RobotBaseClass
     methods
 %% Constructor
         function self = BrushBot(baseTr)
+            if nargin < 3
+                if nargin == 2
+                    error('If you set useTool you must pass in the toolFilename as well');
+                elseif nargin == 0 % Nothing passed
+                    baseTr = transl(0,0,0);  
+                end             
+            else % All passed in 
+                self.useTool = useTool;
+                toolTrData = load([toolFilename,'.mat']);
+                self.toolTr = toolTrData.tool;
+                self.toolFilename = [toolFilename,'.ply'];
+            end
+          
             self.CreateModel();
-            if nargin == 1			
-				self.model.base = self.model.base.T * baseTr;
-            end          
+			self.model.base = self.model.base.T; %* transl(-1,1.6,0.82) * trotz(pi());
+            self.model.tool = self.toolTr;
             self.PlotAndColourRobot();
+            drawnow
         end
-
 %% CreateModel
         function CreateModel(self)
             link(1) = Link('d',0.2,    'a',0,      'alpha', pi/2 ,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
@@ -22,6 +34,7 @@ classdef BrushBot < RobotBaseClass
             link(3) = Link('d',0.05,         'a',-0.14,'alpha',0,'offset',0,'qlim',[deg2rad(-170),deg2rad(170)]);
             link(4) = Link('d',-0.05,     'a',-0.10,      'alpha',0,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
             link(5) = Link('d',0,     'a',0,      'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+
             self.model = SerialLink(link,'name',self.name);            
         end    
     end
