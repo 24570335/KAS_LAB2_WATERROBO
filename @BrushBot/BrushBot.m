7e25b8df34d295fd4c6c9f15888b095d8c815c44
DESKTOP-VQPT8O8\alexe <alexevergara@gmail.com>
2024-10-24 21:54:05 +1100

    waypoints for brushbot set, fixed brushbot model ish

--- @BrushBot/BrushBot.m
+++ @BrushBot/BrushBot.m
@@ -22,7 +22,7 @@ classdef BrushBot < RobotBaseClass
             end
           
             self.CreateModel();
-			self.model.base = self.model.base.T * transl(-1.2,1.6,0.82) * trotz(pi());
+			self.model.base = self.model.base.T * transl(-0.7,1.6,0.82) * trotz(pi());
             self.model.tool = self.toolTr;
             self.PlotAndColourRobot();
             drawnow
@@ -30,10 +30,10 @@ classdef BrushBot < RobotBaseClass
 %% CreateModel
         function CreateModel(self)
             link(1) = Link('d',0.2,    'a',0,      'alpha', pi/2 ,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
-            link(2) = Link('d',-0.08,         'a',-0.20,  'alpha',0,'offset',0,'qlim',[deg2rad(-90),deg2rad(90)]);
-            link(3) = Link('d',0.05,         'a',-0.14,'alpha',0,'offset',0,'qlim',[deg2rad(-170),deg2rad(170)]);
-            link(4) = Link('d',-0.05,     'a',-0.10,      'alpha',0,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
-            link(5) = Link('d',0,     'a',0,      'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+            link(2) = Link('d',-0.08,         'a',-0.22,  'alpha',0,'offset',0,'qlim',[deg2rad(-270),deg2rad(270)]);
+            link(3) = Link('d',0.05,         'a',-0.32,'alpha',0,'offset',0,'qlim',[deg2rad(-170),deg2rad(170)]);
+            link(4) = Link('d',-0.05,     'a',-0.28,      'alpha',0,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+            link(5) = Link('d',0,     'a',0.03,      'alpha',pi/2,'offset',-pi/2,'qlim',[deg2rad(-360),deg2rad(360)]);
 
             self.model = SerialLink(link,'name',self.name);            
         end    
--- Wash_script.m
+++ Wash_script.m
@@ -6,40 +6,42 @@ r = UR3e_adjusted;
                  2*pi*7/18,-1/12,1/12,-pi/2,2*pi,0;
                  pi/4,0,0,pi/-2,2*pi,0;];
 
-    waypoints2 = [0,0,0;
-                 0,0,0;
-                 0,0,0;];
+    waypoints2 = [0,-pi/2,0,0,0;
+                 0,-109*pi/90,pi*19/90,0,0;
+                 0,-pi/2,0,0,0;
+                 0,-109*pi/90,pi*19/90,0,0;];
 br = BrushBot;
 q0 = [0,0,0,0,0,0];
+q0_brush = [0,0,0,0,0]
 steps = 25;
 
-q1 = waypoints(1,:);
-qMat = jtraj(q0, q1, steps);
-
-for j=1:steps
-    r.model.animate(qMat(j, :)) % move to bottle
-    drawnow;
-end
-q2 = waypoints(2,:);
-qMat = jtraj(q1,q2,steps);
-
-for k=1:steps
-    r.model.animate(qMat(k,:))% move to sink
-    drawnow;
-end
-
-q3 = waypoints(3,:);
-qMat = jtraj(q2,q3,steps);
-
-for l=1:steps
-    r.model.animate(qMat(l,:)) % tip
-    drawnow;
-end
-q4 = waypoints(4,:);
-qMat = jtraj(q3,q4,steps);
-
-for m=1:steps
-    r.model.animate(qMat(l,:))%to dry
-    drawnow;
-end
+% q1 = waypoints(1,:);
+% qMat = jtraj(q0, q1, steps);
+% 
+% for j=1:steps
+%     r.model.animate(qMat(j, :)) % move to bottle
+%     drawnow;
+% end
+% q2 = waypoints(2,:);
+% qMat = jtraj(q1,q2,steps);
+% 
+% for k=1:steps
+%     r.model.animate(qMat(k,:))% move to sink
+%     drawnow;
+% end
+% 
+% q3 = waypoints(3,:);
+% qMat = jtraj(q2,q3,steps);
+% 
+% for l=1:steps
+%     r.model.animate(qMat(l,:)) % tip
+%     drawnow;
+% end
+% q4 = waypoints(4,:);
+% qMat = jtraj(q3,q4,steps);
+% 
+% for m=1:steps
+%     r.model.animate(qMat(l,:))%to dry
+%     drawnow;
+% end
 
