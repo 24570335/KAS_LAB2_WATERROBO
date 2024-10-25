b354738fb57a68fe0515d057c1c370ac6dc4f53a
Sophie <sophie.l.vuillemin@student.uts.edu.au>
2024-10-25 14:39:45 +1100

    Bottle mesh moves with the expected UR3e robot trajectory

--- Gripper/Gripper.m
+++ Gripper/Gripper.m
@@ -5,17 +5,26 @@ classdef Gripper < RobotBaseClass
     end
     
     methods
-%% Constructor
+        %% Constructor
         function self = Gripper(baseTr)
+            % Prompt the user to select the folder containing the .ply files
+            folderPath = uigetdir;
+            if folderPath ~= 0
+                plyFilesFolder = folderPath;
+                % Use the selected folder path for the ply files
+                self.plyFileNameStem = fullfile(plyFilesFolder, 'Gripper');
+            else
+                error('No folder selected for PLY files.');
+            end
+
             self.CreateModel();
-			self.model.base = self.model.base.T * transl(0,0,0) * trotz(pi());
-            self.model.tool = self.toolTr;
-            %self.PlotAndColourRobot();
+            self.PlotAndColourRobot();
             drawnow
         end
-%% CreateModel
+        
+        %% CreateModel
         function CreateModel(self)
-            link(1) = Link('d',0.2,    'a',0,      'alpha', pi/2 ,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+            link(1) = Link('d',0,    'a',0,      'alpha', pi/2 ,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
             link(2) = Link('d',-0.08,         'a',-0.22,  'alpha',0,'offset',0,'qlim',[deg2rad(-270),deg2rad(270)]);
             link(3) = Link('d',0.05,         'a',-0.32,'alpha',0,'offset',0,'qlim',[deg2rad(-170),deg2rad(170)]);
             link(4) = Link('d',-0.05,     'a',-0.28,      'alpha',0,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
--- Wash_script.m
+++ Wash_script.m
@@ -1,9 +1,31 @@
+% Initialising robot and base:
 hold on
 kitchenEnvironment;
 
+%Loading bottle body and cap:
+% Setting up BOTTLE BODY and applying transformations
+bottleBody = PlaceObject('BottleBody3.ply', [0,0,0]);
+vertsBody = get(bottleBody, 'Vertices');
+vertsHomogeneousBody = [vertsBody, ones(size(vertsBody, 1), 1)];
+initialPositionBody = [-1.1,1.1,.82];  % Updated x,y,z position
+rotationTransformBody = transl(initialPositionBody)*trotx(pi/2);  % Rotate 
+transformedVertsBody = (rotationTransformBody * vertsHomogeneousBody')';
+set(bottleBody, 'Vertices', transformedVertsBody(:, 1:3));
+
+
+% Setting up BOTTLE CAP and applying transformations
+bottleCap = PlaceObject('BottleCap3.ply', [0,0,0]);  % Place on top of table, on top of BOTTLE BODY (adjust coordinates as needed
+vertsCap = get(bottleCap, 'Vertices');
+vertsHomogeneousCap = [vertsCap, ones(size(verts, 1), 1)];
+initialPositionCap = [-1.1,1.1,1];  % Updated x,y,z position
+rotationTransformCap = transl(initialPositionCap)*trotx(pi/2);  % Rotate 
+transformedVertsCap = (rotationTransformCap * vertsHomogeneousCap')';
+set(bottleCap, 'Vertices', transformedVertsCap(:, 1:3));
+
+
 r = UR3e_adjusted;
-waypoints = [71*pi/180,0,0,0,0,pi/4;
-             pi,-1/12,1/12,-pi/2,3*pi/2,0;
+waypoints = [71*pi/180,0,0,0,0,0;
+             pi,-1/12,1/12,-pi/2,3*pi/2,-pi/2;
              2*pi*7/18,-1/12,1/12,-pi/2,2*pi,0;
              pi/4,0,0,pi/-2,2*pi,0;];
 
@@ -13,11 +35,15 @@ waypoints2 = [0,-pi/2,0,0,0;
              0,-pi/2,0,0,0;
              0,-109*pi/90,pi*19/90,0,0;];
 
-grip = Gripper;
-% Testing gripper ply files:
-PlaceObject('GripperLink5.ply',[0,-1.5,0.15]);
-PlaceObject('GripperLink3.ply',[0,-0.5,0.15]);
-PlaceObject('GripperLink1.ply',[0,-1,0.15]);
+
+% THIS IS NOT WORKING WELL:
+%grip = Gripper;
+%grip.model.teach
+
+% Gripper ply files OPEN!:
+%PlaceObject('GripperLink5.ply',[0,-1.5,0.15]);
+%PlaceObject('GripperLink3.ply',[0,-0.5,0.15]);
+%PlaceObject('GripperLink1.ply',[0,-1,0.15]);
 
 q0 = [0,0,0,0,0,0];
 q0_brush = [0,0,0,0,0];
@@ -35,15 +61,36 @@ end
 q2 = waypoints(2,:);
 qMat = jtraj(q1,q2,steps);
 for k=1:steps
-    r.model.animate(qMat(k,:))% Animating the movement to sink
+    r.model.animate(qMat(k,:));% Animating the movement to sink
+
+    lastLinkBody = r.model.fkine(qMat(k,:));
+    transformedVertsBody = (lastLinkBody.T * vertsHomogeneousBody')'; % Multiplying new transform by homogenous vertices matrix
+    set(bottleBody, 'Vertices', transformedVertsBody(:, 1:3));
+
+    lastLinkCap = r.model.fkine(qMat(k,:));
+    lastLinkCap = lastLinkCap.T * transl(0, 0.18, 0);
+    transformedVertsCap = (lastLinkCap * vertsHomogeneousCap')'; % Multiplying new transform by homogenous vertices matrix
+    set(bottleCap, 'Vertices', transformedVertsCap(:, 1:3));
+
     drawnow;
 end
+
 
 % Moving UR3e to tipping position
 q3 = waypoints(3,:);
 qMat = jtraj(q2,q3,steps);
 for l=1:steps
     r.model.animate(qMat(l,:)) % Animating the movement of a tipping position
+
+    lastLinkBody = r.model.fkine(qMat(l,:));
+    transformedVertsBody = (lastLinkBody.T * vertsHomogeneousBody')'; % Multiplying new transform by homogenous vertices matrix
+    set(bottleBody, 'Vertices', transformedVertsBody(:, 1:3));
+
+    lastLinkCap = r.model.fkine(qMat(l,:));
+    lastLinkCap = lastLinkCap.T * transl(0, 0.18, 0);
+    transformedVertsCap = (lastLinkCap * vertsHomogeneousCap')'; % Multiplying new transform by homogenous vertices matrix
+    set(bottleCap, 'Vertices', transformedVertsCap(:, 1:3));
+
     drawnow;
 end
 
@@ -52,6 +99,16 @@ q4 = waypoints(4,:);
 qMat = jtraj(q3,q4,steps);
 for m=1:steps
     r.model.animate(qMat(m,:)) % Animating the movement to dry bottle
+
+    lastLinkBody = r.model.fkine(qMat(m,:));
+    transformedVertsBody = (lastLinkBody.T * vertsHomogeneousBody')'; % Multiplying new transform by homogenous vertices matrix
+    set(bottleBody, 'Vertices', transformedVertsBody(:, 1:3));
+
+    lastLinkCap = r.model.fkine(qMat(m,:));
+    lastLinkCap = lastLinkCap.T * transl(0, 0.18, 0);
+    transformedVertsCap = (lastLinkCap * vertsHomogeneousCap')'; % Multiplying new transform by homogenous vertices matrix
+    set(bottleCap, 'Vertices', transformedVertsCap(:, 1:3));
+
     drawnow;
 end
 
